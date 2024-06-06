0367781e469606f95a7926e00ae25e6a45f3faf0
AlVoro25 <yahyaev.tamirlan@yandex.ru>
2024-06-06 17:39:59 +0300

    Final commit. All features are coompleted

--- src/main/java/yahyaev/petsn/controllers/MainController.java
+++ src/main/java/yahyaev/petsn/controllers/MainController.java
@@ -3,10 +3,7 @@ package yahyaev.petsn.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import yahyaev.petsn.model.Post;
 import yahyaev.petsn.repositories.PostRepository;
 
@@ -63,6 +60,12 @@ public class MainController {
         }
         postRepository.save(post);
         return "redirect:/view-post/" + post.getId();
+    }
+
+    @GetMapping("/delete-post/{id}")
+    public String deletePost(@PathVariable long id) {
+        postRepository.deleteById(id);
+        return "redirect:/main";
     }
 
 }
