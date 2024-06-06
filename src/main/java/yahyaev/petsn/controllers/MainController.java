7ad133cac4d6c37d936e919aa44aacfc8d7c26dc
AlVoro25 <yahyaev.tamirlan@yandex.ru>
2024-06-06 12:14:45 +0300

    Created logic for adding new posts

--- src/main/java/yahyaev/petsn/controllers/MainController.java
+++ src/main/java/yahyaev/petsn/controllers/MainController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import yahyaev.petsn.model.Post;
 import yahyaev.petsn.repositories.PostRepository;
 
@@ -22,7 +24,17 @@ public class MainController {
 
     @GetMapping("/new-post")
     public String newPost(Model model) {
+        model.addAttribute("post", new Post());
         return "new-post";
+    }
+
+    @PostMapping("/new-post")
+    public String addNewPost(@ModelAttribute("post") Post post) {
+        String preview = post.getPreview();
+        preview += "...";
+        post.setPreview(preview);
+        postRepository.save(post);
+        return "redirect:/main";
     }
 
 }
